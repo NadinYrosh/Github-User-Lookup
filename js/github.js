d5627e3ee5c240bb0e382fdaeb481407fc047dee
Nadiya Yeroshkina <nadin.yeroshkina@gmail.com>
2016-05-27 14:34:24 -0700

    display list of repos

--- js/github.js
+++ js/github.js
@@ -14,7 +14,7 @@ exports.Repo.prototype.getRepos = function(userName,public_repos){
   });
 
 
-
+/// display list of repositories
   $.get('https://api.github.com/users/' + userName + '/repos?access_token=' + apiKey).then(function(response){
     for(var i = 0; i < response.length; i++){
 
